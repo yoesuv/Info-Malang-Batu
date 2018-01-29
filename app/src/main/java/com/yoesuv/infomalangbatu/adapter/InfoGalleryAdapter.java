76453ad3aaabc5a34fbcae1b3049daf2339d574b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-01-29 17:00:04 +0700

    improve adapter :racehorse: :penguin:

--- app/src/main/java/com/yoesuv/infomalangbatu/adapter/InfoGalleryAdapter.java
+++ app/src/main/java/com/yoesuv/infomalangbatu/adapter/InfoGalleryAdapter.java
@@ -1,6 +1,7 @@
 package com.yoesuv.infomalangbatu.adapter;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,30 +19,34 @@ public class InfoGalleryAdapter extends ArrayAdapter<InfoGallery> {
         super(context, resource);
     }
 
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         ThumbnailHolder holder;
         if(convertView==null){
             holder = new ThumbnailHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.view_gallery_thumbnail, parent, false);
-            holder.thumbnail = (ImageView) convertView.findViewById(R.id.image_gallery_thumbnail);
+            holder.thumbnail = convertView.findViewById(R.id.image_gallery_thumbnail);
             convertView.setTag(holder);
         }else{
             holder = (ThumbnailHolder) convertView.getTag();
         }
 
         final ThumbnailHolder tmp = holder;
-        Picasso.with(getContext()).load(getItem(position).getThumbnail()).placeholder(R.drawable.img_default).into(holder.thumbnail, new Callback() {
-            @Override
-            public void onSuccess() {
+        InfoGallery ig = getItem(position);
+        if(ig!=null) {
+            Picasso.with(getContext()).load(ig.getThumbnail()).placeholder(R.drawable.img_default).into(holder.thumbnail, new Callback() {
+                @Override
+                public void onSuccess() {
 
-            }
+                }
 
-            @Override
-            public void onError() {
-                tmp.thumbnail.setImageResource(R.drawable.img_default);
-            }
-        });
+                @Override
+                public void onError() {
+                    tmp.thumbnail.setImageResource(R.drawable.img_default);
+                }
+            });
+        }
 
         return convertView;
     }
--- app/src/main/java/com/yoesuv/infomalangbatu/adapter/ListPlaceAdapter.java
+++ app/src/main/java/com/yoesuv/infomalangbatu/adapter/ListPlaceAdapter.java
@@ -1,6 +1,7 @@
 package com.yoesuv.infomalangbatu.adapter;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,35 +20,38 @@ public class ListPlaceAdapter extends ArrayAdapter<ListPlace> {
         super(context, resource);
     }
 
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         ViewHolder holder;
         if(convertView==null){
             holder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.view_list_place, parent, false);
-            holder.thumbnail = (ImageView) convertView.findViewById(R.id.imageView_thumbnail);
-            holder.nama = (TextView) convertView.findViewById(R.id.textView_nama);
-            holder.lokasi = (TextView) convertView.findViewById(R.id.textView_lokasi);
+            holder.thumbnail = convertView.findViewById(R.id.imageView_thumbnail);
+            holder.nama = convertView.findViewById(R.id.textView_nama);
+            holder.lokasi = convertView.findViewById(R.id.textView_lokasi);
             convertView.setTag(holder);
         }else{
             holder = (ViewHolder) convertView.getTag();
         }
 
-        holder.nama.setText(getItem(position).getNama());
-        holder.lokasi.setText(getItem(position).getLokasi());
+        ListPlace lp = getItem(position);
+        if(lp!=null) {
+            holder.nama.setText(lp.getNama());
+            holder.lokasi.setText(lp.getLokasi());
+            final ViewHolder tmp = holder;
+            Picasso.with(getContext()).load(lp.getThumbnail()).placeholder(R.drawable.img_default).into(holder.thumbnail, new Callback() {
+                @Override
+                public void onSuccess() {
 
-        final ViewHolder tmp = holder;
-        Picasso.with(getContext()).load(getItem(position).getThumbnail()).placeholder(R.drawable.img_default).into(holder.thumbnail, new Callback() {
-            @Override
-            public void onSuccess() {
+                }
 
-            }
-
-            @Override
-            public void onError() {
-                tmp.thumbnail.setImageResource(R.drawable.img_default);
-            }
-        });
+                @Override
+                public void onError() {
+                    tmp.thumbnail.setImageResource(R.drawable.img_default);
+                }
+            });
+        }
 
         return convertView;
     }
