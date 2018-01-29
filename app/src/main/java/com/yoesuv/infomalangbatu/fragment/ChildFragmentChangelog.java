00d4b78c204bbc76eff1e805a7d91220da24ff3c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-01-29 16:43:28 +0700

    add changelog :pencil: :penguin:

--- app/src/main/java/com/yoesuv/infomalangbatu/fragment/ChildFragmentChangelog.java
+++ app/src/main/java/com/yoesuv/infomalangbatu/fragment/ChildFragmentChangelog.java
@@ -3,7 +3,6 @@ package com.yoesuv.infomalangbatu.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +10,9 @@ import android.widget.TextView;
 
 import com.yoesuv.infomalangbatu.R;
 import com.yoesuv.infomalangbatu.utils.ActivityHelper;
+import com.yoesuv.infomalangbatu.utils.GravityLightTextView;
 
 public class ChildFragmentChangelog extends Fragment {
-
-    private View v;
-    private TextView tv1,tv2,tv3,tv4,tv5;
 
     public static ChildFragmentChangelog getInstance(){
         return new ChildFragmentChangelog();
@@ -24,22 +21,25 @@ public class ChildFragmentChangelog extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.fragment_changelog, container, false);
+        View v = inflater.inflate(R.layout.fragment_changelog, container, false);
 
-        tv1 = (TextView) v.findViewById(R.id.textView_ver_1);
+        TextView tv1 = v.findViewById(R.id.textView_ver_1);
         tv1.setText(ActivityHelper.fromHtml(getResources().getString(R.string.ver_1_info)));
 
-        tv2 = (TextView) v.findViewById(R.id.textView_ver_2_info);
+        TextView tv2 = v.findViewById(R.id.textView_ver_2_info);
         tv2.setText(ActivityHelper.fromHtml(getResources().getString(R.string.ver_2_info)));
 
-        tv3 = (TextView) v.findViewById(R.id.textView_ver_3_info);
+        TextView tv3 = v.findViewById(R.id.textView_ver_3_info);
         tv3.setText(ActivityHelper.fromHtml(getResources().getString(R.string.ver_3_info)));
 
-        tv4 = (TextView) v.findViewById(R.id.textView_ver_4_info);
+        TextView tv4 = v.findViewById(R.id.textView_ver_4_info);
         tv4.setText(ActivityHelper.fromHtml(getResources().getString(R.string.ver_4_info)));
 
-        tv5 = (TextView) v.findViewById(R.id.textView_ver_5_info);
+        TextView tv5 = v.findViewById(R.id.textView_ver_5_info);
         tv5.setText(ActivityHelper.fromHtml(getResources().getString(R.string.ver_5_info)));
+
+        GravityLightTextView tv6 = v.findViewById(R.id.textView_ver_6_info);
+        tv6.setText(ActivityHelper.fromHtml(getResources().getString(R.string.ver_6_info)));
 
         return v;
     }
