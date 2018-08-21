a50c6b7bf09abfa25e1119ac038a1d44c77ff237
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-08-21 10:04:27 +0700

    :tractor: :penguin: setup main layout

---
+++ app/src/main/java/com/yoesuv/infomalangbatu/widgets/TextViewPacifico.java
@@ -0,0 +1,29 @@
+package com.yoesuv.infomalangbatu.widgets;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+public class TextViewPacifico extends AppCompatTextView {
+
+    public TextViewPacifico(Context context) {
+        super(context);
+        init();
+    }
+
+    public TextViewPacifico(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public TextViewPacifico(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init(){
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(),"fonts/pacifico.ttf");
+        setTypeface(tf);
+    }
+}
