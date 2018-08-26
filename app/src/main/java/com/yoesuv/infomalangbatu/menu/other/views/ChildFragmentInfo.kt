13d501fa3ec37ac6e4bfd1c68a7f308c6a375da8
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-08-26 07:49:55 +0700

    :pencil: :penguin: set icon app info

--- app/src/main/java/com/yoesuv/infomalangbatu/menu/other/views/ChildFragmentInfo.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/menu/other/views/ChildFragmentInfo.kt
@@ -1,11 +1,14 @@
 package com.yoesuv.infomalangbatu.menu.other.views
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.yoesuv.infomalangbatu.BuildConfig
 import com.yoesuv.infomalangbatu.R
+import com.yoesuv.infomalangbatu.databinding.ChildFragmentInfoBinding
 
 class ChildFragmentInfo: Fragment() {
 
@@ -16,8 +19,9 @@ class ChildFragmentInfo: Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = inflater.inflate(R.layout.child_fragment_info, container, false)
-        return view
+        val binding: ChildFragmentInfoBinding = DataBindingUtil.inflate(inflater, R.layout.child_fragment_info, container, false)
+        binding.textViewVersion.text = getString(R.string.info_app_version, BuildConfig.VERSION_NAME)
+        return binding.root
     }
 
 }
