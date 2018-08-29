80ff85676fb67da37683377fe5fc2b424ea4d933
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-08-29 10:46:14 +0700

    :sparkles: :penguin: set fragment libraries

--- app/src/main/java/com/yoesuv/infomalangbatu/menu/other/viewmodels/ChildFragmentLibrariesViewModel.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/menu/other/viewmodels/ChildFragmentLibrariesViewModel.kt
@@ -3,6 +3,7 @@ package com.yoesuv.infomalangbatu.menu.other.viewmodels
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.content.Context
+import com.yoesuv.infomalangbatu.R
 import com.yoesuv.infomalangbatu.menu.other.models.LibraryModel
 
 class ChildFragmentLibrariesViewModel: ViewModel() {
@@ -12,8 +13,17 @@ class ChildFragmentLibrariesViewModel: ViewModel() {
 
     fun setupData(context: Context){
         listLibraries.clear()
-        listLibraries.add(LibraryModel("custom title", "custom url", "custom license", false))
-        listLibraries.add(LibraryModel("custom title 2", "custom url", "custom license", true))
+        listLibraries.add(LibraryModel(context.getString(R.string.google_direction), context.getString(R.string.google_direction_url), context.getString(R.string.google_direction_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.glide), context.getString(R.string.glide_url), context.getString(R.string.glide_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.icons8), context.getString(R.string.icons8_url), context.getString(R.string.icons8_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.navigation_tab_strip), context.getString(R.string.navigation_tab_strip_url), context.getString(R.string.navigation_tab_strip_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.retrofit), context.getString(R.string.retrofit_url), context.getString(R.string.retrofit_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.rxAndroid), context.getString(R.string.rxAndroid_url), context.getString(R.string.rxAndroid_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.rxjava), context.getString(R.string.rxjava_url), context.getString(R.string.rxjava_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.rx_permission), context.getString(R.string.rx_permission_url), context.getString(R.string.rx_permission_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.sdp_android), context.getString(R.string.sdp_android_url), context.getString(R.string.sdp_android_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.ssp_android), context.getString(R.string.ssp_android_url), context.getString(R.string.sdp_android_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.toasty), context.getString(R.string.toasty_url), context.getString(R.string.toasty_license), true))
         listData.value = listLibraries
     }
 
--- app/src/main/java/com/yoesuv/infomalangbatu/menu/other/viewmodels/ItemChangeLogViewModel.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/menu/other/viewmodels/ItemChangeLogViewModel.kt
@@ -8,4 +8,5 @@ import com.yoesuv.infomalangbatu.utils.AppHelper
 class ItemChangeLogViewModel(changeLogModel: ChangeLogModel?): ViewModel() {
     var version: ObservableField<String> = ObservableField(changeLogModel?.title!!)
     var description: ObservableField<String> = ObservableField(AppHelper.fromHtml(changeLogModel?.description!!))
+    var isLast: ObservableField<Boolean> = ObservableField(changeLogModel?.isLast!!)
 }
