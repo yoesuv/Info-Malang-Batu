1447c6d3d5b9edf094f4e9428aabde4a5fcdbbe9
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-09-04 15:52:30 +0700

    :pencil: :penguin: add list libraries

--- app/src/main/java/com/yoesuv/infomalangbatu/menu/other/viewmodels/ChildFragmentLibrariesViewModel.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/menu/other/viewmodels/ChildFragmentLibrariesViewModel.kt
@@ -13,10 +13,12 @@ class ChildFragmentLibrariesViewModel: ViewModel() {
 
     fun setupData(context: Context){
         listLibraries.clear()
+        listLibraries.add(LibraryModel(context.getString(R.string.aosp), context.getString(R.string.aosp_url), context.getString(R.string.aosp_license), false))
         listLibraries.add(LibraryModel(context.getString(R.string.google_direction), context.getString(R.string.google_direction_url), context.getString(R.string.google_direction_license), false))
         listLibraries.add(LibraryModel(context.getString(R.string.glide), context.getString(R.string.glide_url), context.getString(R.string.glide_license), false))
         listLibraries.add(LibraryModel(context.getString(R.string.icons8), context.getString(R.string.icons8_url), context.getString(R.string.icons8_license), false))
         listLibraries.add(LibraryModel(context.getString(R.string.navigation_tab_strip), context.getString(R.string.navigation_tab_strip_url), context.getString(R.string.navigation_tab_strip_license), false))
+        listLibraries.add(LibraryModel(context.getString(R.string.okhttp), context.getString(R.string.okhttp_url), context.getString(R.string.okhttp_license), false))
         listLibraries.add(LibraryModel(context.getString(R.string.retrofit), context.getString(R.string.retrofit_url), context.getString(R.string.retrofit_license), false))
         listLibraries.add(LibraryModel(context.getString(R.string.rxAndroid), context.getString(R.string.rxAndroid_url), context.getString(R.string.rxAndroid_license), false))
         listLibraries.add(LibraryModel(context.getString(R.string.rxjava), context.getString(R.string.rxjava_url), context.getString(R.string.rxjava_license), false))
