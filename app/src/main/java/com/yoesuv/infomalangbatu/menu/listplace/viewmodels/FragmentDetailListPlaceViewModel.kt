dc8aa3e814553610936462af594d3af5adb10920
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-07-30 11:16:28 +0700

    :sparkles: fragment detail list place

--- app/src/main/java/com/yoesuv/infomalangbatu/menu/listplace/viewmodels/FragmentDetailListPlaceViewModel.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/menu/listplace/viewmodels/FragmentDetailListPlaceViewModel.kt
@@ -1,7 +1,12 @@
 package com.yoesuv.infomalangbatu.menu.listplace.viewmodels
 
 import android.app.Application
+import androidx.databinding.ObservableField
 import androidx.lifecycle.AndroidViewModel
+import com.yoesuv.infomalangbatu.menu.listplace.models.PlaceModel
 
-class FragmentDetailListPlaceViewModel(application: Application) : AndroidViewModel(application) {
+class FragmentDetailListPlaceViewModel(application: Application, placeModel: PlaceModel?) : AndroidViewModel(application) {
+    var title: ObservableField<String> = ObservableField(placeModel?.name!!)
+    var description: ObservableField<String> = ObservableField(placeModel?.description!!)
+    var imageUrl: ObservableField<String> = ObservableField(placeModel?.image!!)
 }
--- app/src/main/java/com/yoesuv/infomalangbatu/menu/listplace/views/FragmentDetailListPlace.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/menu/listplace/views/FragmentDetailListPlace.kt
@@ -6,16 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.yoesuv.infomalangbatu.R
 import com.yoesuv.infomalangbatu.databinding.FragmentDetailListplaceBinding
+import com.yoesuv.infomalangbatu.menu.listplace.models.PlaceModel
+import com.yoesuv.infomalangbatu.menu.listplace.viewmodels.FragmentDetailListPlaceViewModel
 
 class FragmentDetailListPlace: Fragment() {
 
     private lateinit var binding: FragmentDetailListplaceBinding
+    private lateinit var viewModel: FragmentDetailListPlaceViewModel
+
+    private val args: FragmentDetailListPlaceArgs by navArgs()
+    private var placeModel: PlaceModel? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        placeModel = args.dataDetailListPlace
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail_listplace, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = FragmentDetailListPlaceViewModel(activity!!.application, placeModel)
+        binding.detailListPlace = viewModel
     }
 
 }
