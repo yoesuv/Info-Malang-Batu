995da9a8dabb31eec45e30126453bee27a00d401
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-08-27 11:27:47 +0700

    :pencil: connect API list place

--- app/src/main/java/com/yoesuv/infomalangbatu/main/viewmodels/SplashViewModel.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/main/viewmodels/SplashViewModel.kt
@@ -2,16 +2,41 @@ package com.yoesuv.infomalangbatu.main.viewmodels
 
 import android.app.Application
 import android.content.Context
+
 import androidx.databinding.ObservableField
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+
 import com.yoesuv.infomalangbatu.BuildConfig
 import com.yoesuv.infomalangbatu.R
+import com.yoesuv.infomalangbatu.networks.ServiceFactory
+
+import kotlinx.coroutines.launch
 
 class SplashViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val restApi = ServiceFactory.create()
 
     var version: ObservableField<String> = ObservableField()
 
     fun setVersion(context: Context) {
         version.set(context.getString(R.string.info_app_version, BuildConfig.VERSION_NAME))
+    }
+
+    fun initDataBase(context: Context) {
+        viewModelScope.launch {
+            try {
+                val result = restApi.getListPlace()
+                if (result.isSuccessful) {
+
+                } else {
+
+                }
+            } catch (error: Exception) {
+
+            } finally {
+
+            }
+        }
     }
 }
--- app/src/main/java/com/yoesuv/infomalangbatu/main/views/SplashActivity.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/main/views/SplashActivity.kt
@@ -3,7 +3,7 @@ package com.yoesuv.infomalangbatu.main.views
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.yoesuv.infomalangbatu.R
 import com.yoesuv.infomalangbatu.databinding.ActivitySplashBinding
 import com.yoesuv.infomalangbatu.main.viewmodels.SplashViewModel
@@ -17,10 +17,11 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
-        viewModel = ViewModelProviders.of(this).get(SplashViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
         binding.splash = viewModel
 
         viewModel.setVersion(this)
+        viewModel.initDataBase(this)
     }
 
 }
