65279d2ac35c90348647b29316b277f569b3ebc7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-03-29 08:51:39 +0700

    :fire: remove fabric library

--- app/src/main/java/com/yoesuv/infomalangbatu/App.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/App.kt
@@ -3,9 +3,7 @@ package com.yoesuv.infomalangbatu
 import android.content.Context
 import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
-import com.crashlytics.android.Crashlytics
 import com.yoesuv.infomalangbatu.utils.PreferencesHelper
-import io.fabric.sdk.android.Fabric
 
 class App: MultiDexApplication() {
 
@@ -21,9 +19,6 @@ class App: MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         prefHelper = PreferencesHelper(this)
-        if (BuildConfig.FLAVOR.equals("production", true)) {
-            Fabric.with(this, Crashlytics())
-        }
     }
 
 }
