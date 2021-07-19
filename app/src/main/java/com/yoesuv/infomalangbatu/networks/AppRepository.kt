ade1c08c59e5808b4e7538658d83fa55d9e8fa9a
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-07-19 20:31:27 +0700

    :pencil: async request

--- app/src/main/java/com/yoesuv/infomalangbatu/networks/AppRepository.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/networks/AppRepository.kt
@@ -4,6 +4,7 @@ import com.yoesuv.infomalangbatu.menu.gallery.models.GalleryModel
 import com.yoesuv.infomalangbatu.menu.listplace.models.PlaceModel
 import com.yoesuv.infomalangbatu.menu.maps.models.PinModel
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class AppRepository(private val coroutineScope: CoroutineScope) {
@@ -13,11 +14,11 @@ class AppRepository(private val coroutineScope: CoroutineScope) {
     fun getAppData(onSuccess:(MutableList<PlaceModel>?, MutableList<GalleryModel>?, MutableList<PinModel>?) -> Unit, onError:(Throwable) -> Unit) {
         coroutineScope.launch {
             try {
-                val resultListPlace = restApi.getListPlace()
-                val resultGallery = restApi.getGallery()
-                val resultMapPins = restApi.getMapPins()
+                val resultListPlace = async { restApi.getListPlace() }
+                val resultGallery = async { restApi.getGallery() }
+                val resultMapPins = async { restApi.getMapPins() }
 
-                onSuccess(resultListPlace.body(), resultGallery.body(), resultMapPins.body())
+                onSuccess(resultListPlace.await().body(), resultGallery.await().body(), resultMapPins.await().body())
             } catch (throwable: Throwable) {
                 onError(throwable)
             }
