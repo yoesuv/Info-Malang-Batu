1461d3ae144e3e32db85eb48050dc6d39ce1ae67
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-06-20 11:52:46 +0700

    :wrench: fix title info window

--- app/src/main/java/com/yoesuv/infomalangbatu/menu/maps/adapters/MyCustomInfoWindowAdapter.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/menu/maps/adapters/MyCustomInfoWindowAdapter.kt
@@ -4,25 +4,26 @@ import android.view.ContextThemeWrapper
 import androidx.fragment.app.FragmentActivity
 import android.view.LayoutInflater
 import android.view.View
-import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter
 import com.google.android.gms.maps.model.Marker
 import com.google.android.material.textview.MaterialTextView
 import com.yoesuv.infomalangbatu.R
 import com.yoesuv.infomalangbatu.menu.maps.models.MarkerTag
 
-class MyCustomInfoWindowAdapter(private val activity: FragmentActivity?) : GoogleMap.InfoWindowAdapter {
+class MyCustomInfoWindowAdapter(activity: FragmentActivity?) : InfoWindowAdapter {
 
     private val ctxWrapper = ContextThemeWrapper(activity, R.style.AppTheme)
-    private val mContents: View = LayoutInflater.from(ctxWrapper).inflate(R.layout.custom_info_window, null)
+    private val mContents: View = LayoutInflater.from(ctxWrapper)
+        .inflate(R.layout.custom_info_window, activity?.findViewById(android.R.id.content), false)
 
     override fun getInfoContents(marker: Marker): View {
         return mContents
     }
 
     override fun getInfoWindow(marker: Marker): View {
-        val tag: MarkerTag = marker.tag as MarkerTag
+        val tag = marker.tag as MarkerTag
         if (tag.type == 0) {
-            val tv: MaterialTextView? = activity?.findViewById(R.id.textViewMapLocationName)
+            val tv: MaterialTextView? = mContents.findViewById(R.id.textViewMapLocationName)
             tv?.text = tag.title
         }
         return mContents
