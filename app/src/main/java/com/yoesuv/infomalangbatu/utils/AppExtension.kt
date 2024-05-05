c00b56c628539e1ce48ad5e5ca5392142fe27efb
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-05-05 16:53:05 +0700

    :tractor: apply ksp & update kotlin

--- app/src/main/java/com/yoesuv/infomalangbatu/utils/AppExtension.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/utils/AppExtension.kt
@@ -3,13 +3,20 @@ package com.yoesuv.infomalangbatu.utils
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.GenericTransitionOptions
-import com.yoesuv.infomalangbatu.utils.glide.GlideApp
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.yoesuv.infomalangbatu.R
 
 
 @BindingAdapter("setImageUrl")
 fun AppCompatImageView.setImageUrl(imageUrl: String) {
-    GlideApp.with(context)
+    Glide.with(context)
         .load(imageUrl)
+        .placeholder(R.drawable.placeholder_image)
+        .error(R.drawable.placeholder_image)
+        .format(DecodeFormat.PREFER_ARGB_8888)
+        .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
         .transition(GenericTransitionOptions.with(android.R.anim.fade_in))
         .into(this)
 }
