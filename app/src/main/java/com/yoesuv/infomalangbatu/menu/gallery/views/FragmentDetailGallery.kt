6f4793657fe843244624cb0208a5750885a0430c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-09-21 12:09:30 +0700

    :pencil: update detail gallery

--- app/src/main/java/com/yoesuv/infomalangbatu/menu/gallery/views/FragmentDetailGallery.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/menu/gallery/views/FragmentDetailGallery.kt
@@ -4,40 +4,53 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.yoesuv.infomalangbatu.R
 import com.yoesuv.infomalangbatu.databinding.FragmentDetailGalleryBinding
 import com.yoesuv.infomalangbatu.main.views.MainActivity
 import com.yoesuv.infomalangbatu.menu.gallery.models.GalleryModel
 import com.yoesuv.infomalangbatu.menu.gallery.viewmodels.FragmentDetailGalleryViewModel
 import com.yoesuv.infomalangbatu.utils.bindings.ViewModelFragmentFactory
 
-class FragmentDetailGallery: Fragment() {
+class FragmentDetailGallery : Fragment() {
 
-    private lateinit var binding: FragmentDetailGalleryBinding
+    private var binding: FragmentDetailGalleryBinding? = null
     private val viewModel: FragmentDetailGalleryViewModel by viewModels { ViewModelFragmentFactory(galleryModel as Any) }
 
     private val args: FragmentDetailGalleryArgs by navArgs()
     private var galleryModel: GalleryModel? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         galleryModel = args.dataDetailGallery
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail_gallery, container, false)
-        return binding.root
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        if (binding == null) {
+            binding = FragmentDetailGalleryBinding.inflate(inflater, container, false)
+            binding?.detailGallery = viewModel
+        }
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as MainActivity).hideNavigation(true)
-        binding.detailGallery = viewModel
+        onBackPressed()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         (activity as MainActivity).hideNavigation(false)
+    }
+
+    private fun onBackPressed() {
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            findNavController().navigateUp()
+        }
     }
 
 }
