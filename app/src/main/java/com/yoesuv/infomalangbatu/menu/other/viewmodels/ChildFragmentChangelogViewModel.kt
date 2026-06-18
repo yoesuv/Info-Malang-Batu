11b8746035cb00096bb28ef4976eabf8e3e88fc1
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2026-06-18 10:38:42 +0700

    refactor: extract changelog and library resource arrays into companion constants and streamline setupData
    Replace repetitive add(...) calls in ChildFragmentChangelogViewModel and ChildFragmentLibrariesViewModel with companion-held resource arrays and mapIndexed construction. Centralizes the resource tuples, reduces duplication, and preserves the logic that marks the last entry as selected.

--- app/src/main/java/com/yoesuv/infomalangbatu/menu/other/viewmodels/ChildFragmentChangelogViewModel.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/menu/other/viewmodels/ChildFragmentChangelogViewModel.kt
@@ -12,132 +12,40 @@ class ChildFragmentChangelogViewModel : ViewModel() {
 
     fun setupData(context: Context) {
         listChangelog.clear()
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_18),
-                context.getString(R.string.ver_18_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_17),
-                context.getString(R.string.ver_17_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_16),
-                context.getString(R.string.ver_16_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_15),
-                context.getString(R.string.ver_15_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_14),
-                context.getString(R.string.ver_14_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_13),
-                context.getString(R.string.ver_13_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_12),
-                context.getString(R.string.ver_12_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_11),
-                context.getString(R.string.ver_11_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_10),
-                context.getString(R.string.ver_10_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_9),
-                context.getString(R.string.ver_9_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_8),
-                context.getString(R.string.ver_8_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_7),
-                context.getString(R.string.ver_7_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_6),
-                context.getString(R.string.ver_6_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_5),
-                context.getString(R.string.ver_5_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_4),
-                context.getString(R.string.ver_4_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_3),
-                context.getString(R.string.ver_3_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_2),
-                context.getString(R.string.ver_2_info),
-                false,
-            ),
-        )
-        listChangelog.add(
-            ChangeLogModel(
-                context.getString(R.string.ver_1),
-                context.getString(R.string.ver_1_info),
-                true,
-            ),
-        )
+        listChangelog =
+            changelogEntries
+                .mapIndexed { index, resIds ->
+                    ChangeLogModel(
+                        context.getString(resIds[0]),
+                        context.getString(resIds[1]),
+                        index == changelogEntries.lastIndex,
+                    )
+                }.toMutableList()
         listData.value = listChangelog
+    }
+
+    companion object {
+        // Each entry: [titleRes, descriptionRes]
+        private val changelogEntries =
+            listOf(
+                intArrayOf(R.string.ver_18, R.string.ver_18_info),
+                intArrayOf(R.string.ver_17, R.string.ver_17_info),
+                intArrayOf(R.string.ver_16, R.string.ver_16_info),
+                intArrayOf(R.string.ver_15, R.string.ver_15_info),
+                intArrayOf(R.string.ver_14, R.string.ver_14_info),
+                intArrayOf(R.string.ver_13, R.string.ver_13_info),
+                intArrayOf(R.string.ver_12, R.string.ver_12_info),
+                intArrayOf(R.string.ver_11, R.string.ver_11_info),
+                intArrayOf(R.string.ver_10, R.string.ver_10_info),
+                intArrayOf(R.string.ver_9, R.string.ver_9_info),
+                intArrayOf(R.string.ver_8, R.string.ver_8_info),
+                intArrayOf(R.string.ver_7, R.string.ver_7_info),
+                intArrayOf(R.string.ver_6, R.string.ver_6_info),
+                intArrayOf(R.string.ver_5, R.string.ver_5_info),
+                intArrayOf(R.string.ver_4, R.string.ver_4_info),
+                intArrayOf(R.string.ver_3, R.string.ver_3_info),
+                intArrayOf(R.string.ver_2, R.string.ver_2_info),
+                intArrayOf(R.string.ver_1, R.string.ver_1_info),
+            )
     }
 }
--- app/src/main/java/com/yoesuv/infomalangbatu/menu/other/viewmodels/ChildFragmentLibrariesViewModel.kt
+++ app/src/main/java/com/yoesuv/infomalangbatu/menu/other/viewmodels/ChildFragmentLibrariesViewModel.kt
@@ -12,70 +12,31 @@ class ChildFragmentLibrariesViewModel : ViewModel() {
 
     fun setupData(context: Context) {
         listLibraries.clear()
-        listLibraries.add(
-            LibraryModel(
-                context.getString(R.string.aosp),
-                context.getString(R.string.aosp_url),
-                context.getString(R.string.aosp_license),
-                false,
-            ),
-        )
-        listLibraries.add(
-            LibraryModel(
-                context.getString(R.string.google_direction),
-                context.getString(R.string.google_direction_url),
-                context.getString(R.string.google_direction_license),
-                false,
-            ),
-        )
-        listLibraries.add(
-            LibraryModel(
-                context.getString(R.string.glide),
-                context.getString(R.string.glide_url),
-                context.getString(R.string.glide_license),
-                false,
-            ),
-        )
-        listLibraries.add(
-            LibraryModel(
-                context.getString(R.string.icons8),
-                context.getString(R.string.icons8_url),
-                context.getString(R.string.icons8_license),
-                false,
-            ),
-        )
-        listLibraries.add(
-            LibraryModel(
-                context.getString(R.string.okhttp),
-                context.getString(R.string.okhttp_url),
-                context.getString(R.string.okhttp_license),
-                false,
-            ),
-        )
-        listLibraries.add(
-            LibraryModel(
-                context.getString(R.string.retrofit),
-                context.getString(R.string.retrofit_url),
-                context.getString(R.string.retrofit_license),
-                false,
-            ),
-        )
-        listLibraries.add(
-            LibraryModel(
-                context.getString(R.string.sdp_android),
-                context.getString(R.string.sdp_android_url),
-                context.getString(R.string.sdp_android_license),
-                false,
-            ),
-        )
-        listLibraries.add(
-            LibraryModel(
-                context.getString(R.string.ssp_android),
-                context.getString(R.string.ssp_android_url),
-                context.getString(R.string.sdp_android_license),
-                true,
-            ),
-        )
+        listLibraries =
+            libraryEntries
+                .mapIndexed { index, resIds ->
+                    LibraryModel(
+                        context.getString(resIds[0]),
+                        context.getString(resIds[1]),
+                        context.getString(resIds[2]),
+                        index == libraryEntries.lastIndex,
+                    )
+                }.toMutableList()
         listData.value = listLibraries
+    }
+
+    companion object {
+        // Each entry: [nameRes, urlRes, licenseRes]
+        private val libraryEntries =
+            listOf(
+                intArrayOf(R.string.aosp, R.string.aosp_url, R.string.aosp_license),
+                intArrayOf(R.string.google_direction, R.string.google_direction_url, R.string.google_direction_license),
+                intArrayOf(R.string.glide, R.string.glide_url, R.string.glide_license),
+                intArrayOf(R.string.icons8, R.string.icons8_url, R.string.icons8_license),
+                intArrayOf(R.string.okhttp, R.string.okhttp_url, R.string.okhttp_license),
+                intArrayOf(R.string.retrofit, R.string.retrofit_url, R.string.retrofit_license),
+                intArrayOf(R.string.sdp_android, R.string.sdp_android_url, R.string.sdp_android_license),
+                intArrayOf(R.string.ssp_android, R.string.ssp_android_url, R.string.sdp_android_license),
+            )
     }
 }
